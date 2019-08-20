a2698b52c2e59e5920f0fc5de6b3a6bd7656fc2f
Ahmed Almuhrij <ahmedalmuhrij@Ahmeds-MacBook-Pro.local>
2019-08-20 18:25:24 +0300

    first week

--- README.txt
+++
@@ -1,1 +0,0 @@
-# Python-Saudi-Dev-Org
---
+++ forth.py
@@ -0,0 +1,47 @@
+c=1
+v= 35443432453434324235
+m= -23423423
+
+print(type(c))
+print(type(v))
+print(type(m))
+
+
+z=1.10
+q=1.0
+w= -35.54
+print(type(z))
+print(type(q))
+print(type(w))
+
+z=35e3
+q=12E4
+w= -87.7e100
+print(type(z))
+print(type(q))
+print(type(w))
+
+z=3+5j
+q=12j
+w= -5j
+print(type(z))
+print(type(q))
+print(type(w))
+
+z=1 #int
+q=12.4 #float
+w= 1j #complex
+
+r= float(z)
+t= int(q)
+y=complex(w)
+print(r)
+print(t)
+print(y)
+print(type(r))
+print(type(t))
+print(type(y))
+
+import random
+print(random.randrange(1,10))
+
